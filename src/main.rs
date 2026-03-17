7b16a4970ae8a0c81892580cd2146db0ea965e81
pherapont <pherapont@yandex.ru>
2026-03-17 10:45:03 +0700

    Fibonacci

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,37 @@
 use std::io;
 
 fn main() {
-    println!("Convert tmperature from Farenhait to Celsius.");
-    println!("Input temperature in Celsius: ");
+    println!{"Fibonacci"};
+    println!{"Put number for fib: "};
 
-    let mut tmp = String::new();
+    let mut input = String::new();
+    let mut num: u32 = 0;
 
     io::stdin()
-        .read_line(&mut tmp)
+        .read_line(&mut input)
         .expect("Wrong input!");
 
-    let tmp: i32 = tmp.trim().parse().expect("Type a number.");
+    num = input.trim().parse().expect("Wrong input");
 
-    let tmp_c = convert_to_celsius(tmp);
-    println!("{tmp_c}");
+    let res: u128 = get_fib(num);
+    println!("Fibonacci of {num}  = {res}");
 }
 
-fn convert_to_celsius (tmp: i32) -> i32 {
-    (tmp - 32) * 5 / 9
+fn get_fib(n: u32) -> u128 {
+    let mut first: u128 = 0;
+    let mut second: u128 = 1;
+    let mut res: u128 = 0;
+
+    if n == 0 { return 0 }
+    else if n == 1 { return 1 }
+
+    let mut i = 2;
+
+    while i <= n {
+        res = first + second;
+        first = second;
+        second = res;
+        i = i + 1;
+    }
+    res
 }
