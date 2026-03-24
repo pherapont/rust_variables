8b42da88b7fd27e60b2061551001cf1c7adff905
pherapont <pherapont34@gmail.com>
2026-03-24 20:43:33 +0700

    Vectors

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,11 @@
-const DAYS: [&str; 12] = [
-    "first", "second", "third", "fourth", "fifth", "sixth",
-    "seventh", "eigth", "ninth", "tenth", "eleventh", "twelfth",
-];
-
-const PRESENTS: [&str; 12] = [
-    "partridge in a pear tree", "turtle doves", "French hens",
-    "calling birds", "gold rings", "geese a-laying",
-    "swans a-swimming", "maids a-milking", "ladies dancing",
-    "lords a-leaping", "pipers piping", "drummers drumming"
-];
-
-const NUMBERS: [&str; 12] = [
-    "One", "Two", "Three", "Four", "Five", "Six",
-    "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"
+const NUMBERS: [i32; 12] = [
+    1, 25, -33, 257, 22, -138, 999, -111, 9, -10, 11, -12
 ];
 
 fn main() {
-    let mut day = 1;
-    while day <= 12 {
-        println!("On the {} day of Christmas my true love sent to me", DAYS[day-1]);
-
-        let mut pres_num = 1;
-        while pres_num < day{
-            println!("{} {}",NUMBERS[day - pres_num], PRESENTS[day - pres_num]);
-            pres_num += 1;
-        }
-
-        let prefix = if day == 1 {"A"} else {"And a"};
-        println!("{prefix} {}.\n", PRESENTS[0]);
-
-        day += 1;
+    let mut nums = Vec::from(NUMBERS);
+    nums.sort();
+    for n in nums{
+        println!("{n}");
     }
 }
