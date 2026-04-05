5fb6de95c17b8dfcd9465b8b0ddac48bf7f7ebc9
pherapont <pherapont34@gmail.com>
2026-04-05 18:20:48 +0700

    List of employes

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,59 @@
-// Convert strings to Pig Latin.
-// The first consonant of each word is moved to the end of the word and ay is added, so first becomes irst-fay.
-// Words that start with a vowel have hay added to the end instead (apple becomes apple-hay).
-// Keep in mind the details about UTF-8 encoding!
+// Using a hash map and vectors, create a text interface to allow a user to add employee names
+// to a department in a company; for example, “Add Sally to Engineering” or “Add Amir to Sales.”
+// Then, let the user retrieve a list of all people in a department or all people in the company by
+// department, sorted alphabetically.
 
+use std::collections::HashMap;
 use std::io;
 
+// TODO добавить Action::Exit
+
+enum Action {
+    Update,
+    Look,
+}
+
 fn main() {
-    println!("Input one word: ");
+    let mut employes: HashMap<String, Vec<String>> = HashMap::new();
 
-    let mut user_input = String::new();
+    loop {
+        let user_aciton: Action = update_or_look();
 
-    io::stdin()
-        .read_line(&mut user_input)
-        .expect("Error in user input");
-
-    let first_ch = &user_input.trim().chars().next().expect("String is empty");
-
-    let word = if is_vowel(first_ch) {
-        format!("{}-hay", &user_input.trim())
-    } else {
-        format!(
-            "{}-{}ay",
-            get_tail_slice(&user_input.trim(), 1),
-            get_slice(&user_input.trim(), 1)
-        )
-    };
-
-    println!("{word}");
-}
-
-fn get_slice(s: &str, char_count: usize) -> &str {
-    match s.char_indices().nth(char_count) {
-        Some((byte_idx, _)) => &s[..byte_idx],
-        None => s, // Вернуть всю строку, если символов меньше
+        match user_aciton {
+            Action::Update => {
+                let department = get_department(&employes);
+                let person: String = get_person(&department);
+                let depart_employes = employes.entry(department).or_insert(Vec::new());
+                depart_employes.push(person);
+            }
+            Action::Look => {
+                println!("{:#?}", employes);
+            }
+        }
     }
 }
 
-fn get_tail_slice(s: &str, first_char: usize) -> &str {
-    match s.char_indices().nth(first_char) {
-        Some((byte_idx, _)) => &s[byte_idx..],
-        None => s, // Вернуть всю строку, если символов меньше
-    }
+fn get_person(department: &str) -> String {
+    "Иванов".to_string()
 }
 
-fn is_vowel(c: &char) -> bool {
-    if matches!(c, 'a' | 'e' | 'i' | 'o' | 'u' | 'A' | 'E' | 'I' | 'O' | 'U') {
-        true
-    } else {
-        false
+fn get_department(employes: &HashMap<String, Vec<String>>) -> String {
+    "Engineering".to_string()
+}
+
+// TODO создать цикл обработки ввода и спаршивать пока не будет правильный ввод (while)
+
+fn update_or_look() -> Action {
+    println!("Выберите действие:");
+    println!("1: Добавление работника в список,");
+    println!("2: Просмотр списка работников.");
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Wrong input!");
+
+    match input.trim() {
+        "1" => Action::Update,
+        "2" => Action::Look,
+        _ => Action::Look,
     }
 }
